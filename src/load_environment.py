102e19c51e1277a68b5bec7e99dff04163101369
Barlaan-ChristineAngela-1102 <christineangelab@nevada.unr.edu>
2024-11-24 15:04:02 -0800

    finalized camera positioning of Interrogation Room

--- src/load_environment.py
+++ src/load_environment.py
@@ -1,45 +1,18 @@
-from math import pi, sin, cos
-
-from direct.showbase.ShowBase import ShowBase, Camera
-from direct.task import Task
-from panda3d.core import Point3
-import direct.directbase.DirectStart 
-from pandac.PandaModules import *
-
+from direct.showbase.ShowBase import ShowBase
 
 class InterrogationRoom(ShowBase):
     def __init__(self):
-        # Initialize the ShowBase class
         ShowBase.__init__(self)
 
-        # Disable default mouse-based camera controls
-        # self.disable_mouse()
+        # Disable deafult mouse controls
+        self.disableMouse()
 
-        # Load the environment model
-        self.scene = self.loader.loadModel("models/neural_noir/converted_room_whole/room.bam")
-        self.scene.reparentTo(self.render)
-        self.scene.setScale(1)
-        self.scene.setPos(0, 0, 0)
-        self.scene.set_scene
+        # Load the room model
+        self.room = self.loader.loadModel("models/neural_noir/converted_room_whole/room.bam")
+        self.room.reparentTo(self.render)
+        self.room.setScale(1)
+        self.room.setPos(5.6, 6, 0.2)
+        self.room.setHpr(0, 0, 0)
 
-        # Position the camera inside the room
-        # self.camera.setPos(200, 0, 5)  # Inside the room, facing forward
-        # self.camera.lookAt(0, 0, 5)    # Point the camera at the center
-
-        # Add the camera spinning task to the task manager
-        # self.taskMgr.add(self.spinCameraTask, "SpinCameraTask")
-
-    # Define a procedure to move the camera
-    # def spinCameraTask(self, task):
-    #     angleDegrees = task.time * 6.0  # Rotate 6 degrees per second
-    #     angleRadians = angleDegrees * (pi / 180.0)
-    #     self.camera.setPos(20 * sin(angleRadians), -20 * cos(angleRadians), 3)
-    #     self.camera.setHpr(angleDegrees, 0, 0)
-    #     return Task.cont
-
-
-# Create an instance of the application
 room = InterrogationRoom()
-
-# Start the Panda3D main loop
 room.run()
