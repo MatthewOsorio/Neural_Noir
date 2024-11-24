357500fc49a6b8e391c0cacf077082de1f0b2e47
1102-Nivera-Evelynne <enivera1@gmail.com>
2024-11-24 14:28:23 -0800

    Moved python files into src
    Moved python files to src and adjusted asset file paths

---
+++ src/main.py
@@ -0,0 +1,21 @@
+from direct.showbase.ShowBase import ShowBase
+from direct.gui.DirectGui import DirectFrame, DirectButton, DGG
+from panda3d.core import TextNode
+from direct.gui.OnscreenImage import OnscreenImage
+from direct.interval.IntervalGlobal import *
+from direct.gui.OnscreenText import OnscreenText
+import time 
+
+import menu
+
+
+class main(ShowBase):
+    def __init__(self):
+        super().__init__()
+
+        self.menuManager = menu.menuManager(self)
+
+        self.menuManager.showMain()
+
+app = main()
+app.run()
