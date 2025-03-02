6d3aaa6496172302618155424effebfacde56e20
Barlaan-ChristineAngela <christineangela.barlaan@gmail.com>
2025-03-02 00:15:04 -0800

    add tests to main

---
+++ src/tests/context.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+# Ensure the parent directory is in sys.path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
+
+from backend.AI_System.AIController import AIController
+from backend.BackendInterface.GameManager import GameManager
+from backend.BiometricSystem.BiometricController import BiometricController
+from backend.Conversation.ConversationModel import ConversationModel
+from backend.SRSystem.SpeechToText import SpeechToText
+from backend.TTSSystem import TextToSpeechController
+from backend.Database.DatabaseController import DatabaseController
---
+++ src/tests/test_GameManager.py
@@ -0,0 +1,22 @@
+import pytest
+from .context import GameManager
+
+@pytest.fixture
+def game_manager():
+    return GameManager()
+
+def test_set_up(game_manager):
+    game_manager.setupGame()
+    
+    assert game_manager._aiController != None
+    assert game_manager._bioController != None
+    assert game_manager._conversation != None
+    assert game_manager._gameState != None
+    assert game_manager._sr != None
+    assert game_manager._tts != None
+    assert game_manager._bioController._aiReference == game_manager._aiController
+    assert game_manager._gameState._aiReference == game_manager._aiController
+    assert game_manager._gameState._biometricReference == game_manager._bioController
+    
+# def test_generateAIResponse(game_manager):
+#     game_manager.
